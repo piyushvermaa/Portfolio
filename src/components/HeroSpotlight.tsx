c483e5432f10845144700cfe9f9742658ae4fb3b
piyushvermaa <piyushsoniiii.07@gmail.com>
2024-03-26 23:19:22 +0530

    first commit

---
+++ src/components/HeroSpotlight.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { cn } from "@/utils/cn";
+import { Spotlight } from "./ui/Spotlight";
+import { ContainerScroll } from "./ui/container-scroll-animation";
+import Image from "next/image";
+import { Typewriter } from "./Typewritter";
+import { BackgroundBeams } from "./ui/background-beams";
+
+function HeroSpotlight() {
+  return (
+    <div className="bg-black">
+      <div className="h-[30rem]  rounded-md  md:items-center md:justify-center  antialiased   overflow-hidden  w-full dark:bg-black mb-5">
+        <Spotlight
+          className="-top-50 left-0 md:left-60 md:-top-20"
+          fill="white"
+        />
+        <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0  mt-20">
+          <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-400 bg-opacity-40 text-[#dedede]">
+            Piyush Verma <br />
+            <Typewriter />
+          </h1>
+          <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+            Welcome to my portfolio! I'm Piyush, a dedicated BTech student at
+            GLA University with a fervent passion for coding, web development,
+            and competitive programming Explore my projects and achievements to witness
+            my commitment to excellence in the world of technology.
+          </p>
+        </div>
+      </div>
+      <div className="mt-[-15rem] ">
+        <ContainerScroll titleComponent={<></>}>
+          <Image
+            src={`/hero.jpg`}
+            alt="hero"
+            height={720}
+            width={1400}
+            className="mx-auto rounded-2xl object-cover h-full "
+            draggable={false}
+          />
+        </ContainerScroll>
+      </div>
+      <BackgroundBeams />
+    </div>
+  );
+}
+
+export default HeroSpotlight;
---
+++ src/components/Typewritter.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
+export function Typewriter() {
+  const words = [
+    {
+      text: "Keystrokes",
+    },
+    {
+      text: "Shaping",
+    },
+    {
+      text: "Tomorrow's",
+    },
+    {
+      text: "Possibilities",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+    
+  ];
+  return (
+    
+      <div className="flex justify-center">
+      <TypewriterEffectSmooth words={words} />
+      </div>
+      
+    
+  );
+}
