69c8faa0411c3bf510508d3e022c39e425217be4
piyushvermaa <piyushsoniiii.07@gmail.com>
2024-03-27 13:12:41 +0530

    style

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,48 +5,51 @@ import Image from "next/image";
 const Projects = () => {
   const content = [
     {
-      title: "Collaborative Code Editor",
+      title: "Coders Ground",
       description:
         "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
       content: (
-        <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-          Collaborative Code Editor
+        <div className="h-full w-full  flex items-center justify-center text-white">
+          <Image
+            src="/Coderground.png"
+            width={2000}
+            height={1500}
+            className="h-full w-full object-fit"
+            alt="Coders Ground"
+          />
         </div>
       ),
+      imgurl: ""
     },
     {
-      title: "Real time changes",
+      title: "Dashboard",
       description:
-        "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
+        "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
       content: (
         <div className="h-full w-full  flex items-center justify-center text-white">
           <Image
             src="/linear.webp"
-            width={300}
-            height={300}
+            width={1500}
+            height={1500}
             className="h-full w-full object-cover"
-            alt="linear board demo"
+            alt="DashBoard"
           />
         </div>
       ),
     },
     {
-      title: "Version control",
+      title: "Weather App",
       description:
         "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
       content: (
-        <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
-          Version control
-        </div>
-      ),
-    },
-    {
-      title: "Running out of content",
-      description:
-        "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
-      content: (
-        <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-          Running out of content
+        <div className="h-full w-full  flex items-center justify-center text-white">
+          <Image
+            src="/weatherapp.png"
+            width={1500}
+            height={1500}
+            className="h-full w-full object-cover"
+            alt="Weather app"
+          />
         </div>
       ),
     },
